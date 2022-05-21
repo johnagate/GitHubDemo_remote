7d36d9f409555b69ed3f4883583b0fcdd834ef17
John Agate <johnagate@gmail.com>
2022-05-21 16:24:53 -0700

    Add second view

---
+++ GitHubDemo/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by john agate on 5/21/22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
